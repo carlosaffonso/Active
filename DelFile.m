0a403c98406e306b62c9f7eec5ce7890e06a38b4
Carlos Affonso <32414295+carlosaffonso@users.noreply.github.com>
2017-10-17 11:44:45 +0300

    Add files via upload

---
+++ DelFile.m
@@ -0,0 +1,17 @@
+% Function deleted
+function DelFile()
+delete C:\Users\Carlos\ImageSourceTrain\A\*.bmp;
+delete C:\Users\Carlos\ImageSourceTrain\B\*.bmp;
+delete C:\Users\Carlos\ImageSourceTrain\C\*.bmp;
+delete C:\Users\Carlos\ImageSourceTrain\D\*.bmp;
+delete C:\Users\Carlos\ImageSourceTest0\A\*.bmp ;
+delete C:\Users\Carlos\ImageSourceTest0\B\*.bmp ;
+delete C:\Users\Carlos\ImageSourceTest0\C\*.bmp ;
+delete C:\Users\Carlos\ImageSourceTest0\D\*.bmp ;
+delete C:\Users\Carlos\ImageSourceTest1\A\*.bmp ;
+delete C:\Users\Carlos\ImageSourceTest1\B\*.bmp ;
+delete C:\Users\Carlos\ImageSourceTest1\C\*.bmp ;
+delete C:\Users\Carlos\ImageSourceTest1\D\*.bmp ;
+delete C:\Users\Carlos\ImageSourceAlpha\*.bmp ;
+end
+
